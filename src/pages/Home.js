69f68cf9125a14769dd46e714385edf392150555
Karen <karenbanci1@gmail.com>
2024-07-20 01:28:38 -0700

    create hero of homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
-// import "./Home.css";
+import "./Home.css";
+import BtnGreen from "../components/BtnGreen";
 
 function Home() {
   return (
-    <div>
-      <h1>HOME</h1>
-      <p>Learn more about us on this page.</p>
+    <div className="banner">
+      <div class="content">
+        <h1>ELEVATE YOUR ELEGANCE</h1>
+        <p>Unveil Your Beauty with Us</p>
+        <BtnGreen>Our Services</BtnGreen>
+      </div>
+      <img src="images/hero-women-leaf.webp" alt="Women with leaf"></img>
     </div>
   );
 }
