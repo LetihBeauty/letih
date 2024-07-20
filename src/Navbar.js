bd4f19cda7a982c527566a884995670349b4af49
Karen <karenbanci1@gmail.com>
2024-07-20 00:24:19 -0700

    refactor navbar to be more semantic

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,31 +2,29 @@ import "./Navbar.css";
 import data from "./data.json";
 
 function Navbar() {
-  console.log("data", data);
   const navItems = data.navbar;
-  console.log("navItems", navItems);
 
   return (
     <div className="navbar-container">
-      <div className="left-side">
+      <ul>
         {navItems.slice(0, 3).map((item) => (
-          <a key={item.id} href={item.url} className="items">
-            {item.title}
-          </a>
+          <li key={item.id} className="items">
+            <a href={item.url}>{item.title}</a>
+          </li>
         ))}
-      </div>
+      </ul>
       <div className="center-logo">
         <a href="/">
           <img src="https://via.placeholder.com/21" alt="logo" />
         </a>
       </div>
-      <div className="right-side">
+      <ul>
         {navItems.slice(3, 6).map((item) => (
-          <a key={item.id} href={item.url}>
-            {item.title}
-          </a>
+          <li key={item.id}>
+            <a href={item.url}>{item.title}</a>
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 }
