3c53b44774e7dbb0df8034d7ad7ebdd40c6f2fa3
Karen <karenbanci1@gmail.com>
2024-09-13 19:44:50 -0700

    config contentful keys api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { createClient } from "contentful";
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
@@ -10,6 +12,25 @@ import Gallery from "./pages/Gallery";
 import Layout from "./components/Layout";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const client = createClient({
+      space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
+      accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN,
+    });
+
+    client
+      .getEntries()
+      .then((response) => {
+        setData(response.items);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  console.log("Space ID:", process.env.REACT_APP_CONTENTFUL_SPACE_ID);
+  console.log("Access Token:", process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN);
+
   return (
     <Router>
       <Navbar />
@@ -23,6 +44,16 @@ function App() {
           <Route path="/gallery" element={<Gallery />} />
         </Routes>
       </Layout>
+
+      <div>
+        {data ? (
+          data.map((entry) => (
+            <div key={entry.sys.id}>{entry.fields.title}</div>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
     </Router>
   );
 }
