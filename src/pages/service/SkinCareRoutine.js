d006281bf340356c953f64444ac747d4ede1abf9
victorfonsecass <victorfonsecass@gmail.com>
2024-09-27 16:31:52 -0300

    The page has been created, populated with data, and part of the CSS has been finalized.

--- src/pages/service/SkinCareRoutine.js
+++ src/pages/service/SkinCareRoutine.js
@@ -1,8 +1,39 @@
 import React from "react";
+import Treatments from "./Treatments.js";
+import SkinCareRoutineStyles from "../service/SkinCareRoutine.module.css"
+import {serviceSkinCareRoutine} from "../../components/constants/index.js"
+
 const SkinCareRoutine = () => {
+  const primaryService = serviceSkinCareRoutine[0];
 
   return (
-    <h1>SkinCare</h1>
+    <div className="containerService">
+      <div className={SkinCareRoutineStyles.ultraformerBanner}>
+        <div className={SkinCareRoutineStyles.centerLogo}>
+          <h1>SKIN CARE ROUTINE</h1>
+        </div>
+      </div>
+      <Treatments
+        customNavWrapperClass={SkinCareRoutineStyles.navbarWrapper}
+        customTitleClass={SkinCareRoutineStyles.title}
+        title={primaryService.title}
+        customDescriptionClass = {SkinCareRoutineStyles.titleDescription}
+        whatIsDescription={primaryService.whatIsDescription}
+        benefits={primaryService.benefits}
+        benefitsDescription={primaryService.benefitsDescription}
+        benefitsRecommendations={primaryService.benefitsRecommendations}
+        timeDescription={primaryService.timeDescription}
+        priceDescription={primaryService.priceDescription}
+        btnComponent={primaryService.btnComponent}
+        imgSrc={primaryService.imgSrc}
+        customClass={SkinCareRoutineStyles.bannerWrapper}
+        customBottomClass={SkinCareRoutineStyles.bannerBottom}
+        customBannerMiddleContentDetailsClass = {SkinCareRoutineStyles.timeAndPrice}
+      />
+      <div  className={SkinCareRoutineStyles.emptySpace}>
+
+      </div>
+    </div>
   );
 };
 export default SkinCareRoutine;
