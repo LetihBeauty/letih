b1f69daff649d40f651c0ebe4dd46227fcf659b3
Karen <karenbanci1@gmail.com>
2024-11-03 22:02:18 -0800

    Refactor ServiceWarning component: Remove unused rich text rendering options

--- src/components/ServiceWarning.jsx
+++ src/components/ServiceWarning.jsx
@@ -4,24 +4,6 @@ import React from "react";
 import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
 import "./ServiceWarning.css"; // Importa o CSS para o estilo Markdown
 
-const options = {
-  renderMark: {
-    bold: (text) => <strong>{text}</strong>,
-    italic: (text) => <em>{text}</em>,
-    underline: (text) => <u>{text}</u>,
-  },
-  renderNode: {
-    paragraph: (node, children) => <p>{children}</p>,
-    "heading-1": (node, children) => <h1>{children}</h1>,
-    "heading-2": (node, children) => <h2>{children}</h2>,
-    "heading-3": (node, children) => <h3>{children}</h3>,
-    "unordered-list": (node, children) => <ul>{children}</ul>,
-    "ordered-list": (node, children) => <ol>{children}</ol>,
-    "list-item": (node, children) => <li>{children}</li>,
-    blockquote: (node, children) => <blockquote>{children}</blockquote>,
-  },
-};
-
 const ServiceWarning = ({ alertMessage }) => {
   if (!alertMessage || !alertMessage.nodeType) {
     return <p>No warnings available.</p>;
