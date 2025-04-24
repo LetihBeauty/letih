cbd2508d5cae2c97b465deb97b9ae0b35b38c69d
Karen <karenbanci1@gmail.com>
2025-04-24 14:36:02 -0700

    Remove Layout.css and refactor Layout component to use Footer component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,10 @@
 import React from "react";
-import "./Layout.css";
+// import "./Layout.css";
 import data from "../data.json";
 import { Link, useLocation } from "react-router-dom";
 import { footerLinks } from "../components/constants/index.js";
 import Navbar from "./Navbar.js";
+import Footer from "./Footer.js";
 
 // import { Link } from "react-router-dom";
 
@@ -20,55 +21,8 @@ const Layout = ({ children }) => {
       <Navbar />
 
       <main>{children}</main>
-      <footer>
-        <img id="logo" src="images/Logo.svg" alt="" />
-        <div className="footer-menu">
-          <ul className="footer-menu-list">
-            {navItems.slice(0, 6).map((item) => (
-              <li key={item.id} className="items">
-                <Link
-                  className={location.pathname === item.url ? "active" : ""}
-                  to={item.url}
-                >
-                  {item.title}
-                </Link>
-              </li>
-            ))}
-          </ul>
-          <img id="line-footer" src="images/Linefooter.svg" alt="" />
-        </div>
-        <div className="footer-contact">
-          <div className="email-phone">
-            <img src="images/icon-email.svg" alt="" />
-            <a href="mailto:letihbeauty@gmail.com">letihbeauty@gmail.com</a>
-          </div>
-          <div className="email-phone">
-            <img src="images/icon-phone.svg" alt="" />
-            <p>{phone}</p>
-          </div>
-        </div>
-        <ul className=" footer-social">
-          {footerLinks.map((item) => (
-            <li key={item.id}>
-              <a href={item.url} target="_blank" rel="noreferrer">
-                <img src={item.icon} alt={item.title} />
-              </a>
-            </li>
-          ))}
-        </ul>
-        <p id="copyright">
-          COPYRIGHT © <span>{year}</span> {copyright}
-          <br></br>
-          <span>{developedBy.title}</span>{" "}
-          <a href={developedBy.karenUrl} target="_blank" rel="noreferrer">
-            {developedBy.karen}
-          </a>{" "}
-          &
-          <a href={developedBy.victorUrl} target="_blank" rel="noreferrer">
-            {developedBy.victor}
-          </a>
-        </p>
-      </footer>
+
+      <Footer />
     </div>
   );
 };
